7c619d1f09da2520551d98858f23095149c8944e
Fabien Alfred <alfred.fabien@gmail.com>
2015-07-29 11:18:31 +0200

    First commit - HelloServlet > doGet()

---
+++ src/test/HelloServlet.java
@@ -0,0 +1,43 @@
+package test;
+
+import java.io.IOException;
+import java.io.Writer;
+import java.util.Date;
+import java.util.logging.Logger;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloServlet
+ */
+@WebServlet("/HelloServlet")
+public class HelloServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private static final Logger log = Logger.getLogger("Hello");
+	
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		log.info("Requête provenant de "+request.getRemoteAddr());
+		Writer out = response.getWriter();
+		out.write("<html><head><title>Hello</title></head>");
+		out.write("<body>");
+		out.write("<h2>HELLO</h2>");
+		Date date = new Date();
+		out.write("date : "+date);
+		out.write("</body></html>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
